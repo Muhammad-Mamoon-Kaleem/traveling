956bb0f5f2438a88a2f6ffcded5c9212469ab23f
Muhammad-Mamoon-Kaleem <mamoonkaleem199@gmail.com>
2024-11-30 12:30:06 +0500

    Update AddPlace.jsx

--- Admin Panel/src/Pages/Admin/AddPlace.jsx
+++ Admin Panel/src/Pages/Admin/AddPlace.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect,useContext } from "react";
+import { AdminContext } from '../../Contexts/AdminContext';
 import axios from "axios";
 import { toast } from "react-toastify";
 import { useLocation } from 'react-router-dom';
