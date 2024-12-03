5cb23f68a7a95066cde0464d9a58dd51b65dbac9
Muhammad-Mamoon-Kaleem <mamoonkaleem199@gmail.com>
2024-12-03 11:46:35 +0500

    Update myprofile.jsx

--- front_end/src/pages/myprofile.jsx
+++ front_end/src/pages/myprofile.jsx
@@ -92,7 +92,7 @@ const MyProfile = () => {
             : <img
               src={usrData.image || assets.profile_pic}
               alt="Profile"
-              className="w-24 h-24 rounded-full object-cover border-2 border-blue-500"
+              className="w-28 h-28 rounded-full object-cover border-2 border-blue-500"
             />
           }
 
